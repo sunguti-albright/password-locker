99c1e2b6cc2b19148f0e848cbffbd8c4b7ac2063
albright.human <sungutialbright@gmail.com>
2022-04-24 15:17:56 +0300

    add user funcions to the main method

--- password_test.py
+++ password_test.py
@@ -15,5 +15,13 @@ class PasswordTest(unittest.TestCase):
         '''
         self.assertEqual(self.new_user.user_name, "Albright")
         self.assertEqual(self.new_user.password, "yeswecan")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into the user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+        
 if __name__ == '__main__':
     unittest.main()
--- run.py
+++ run.py
@@ -1,1 +1,44 @@
+#!/usr/bin/env python3.6
+from password import User, Credentials # Importing user and credentials classes
+
+def create_user(username,password):
+    '''
+    create_user method to create a new user
+    '''
+    new_user = User(username,password)
+    return new_user
+#The end!
+
+def save_user(user):
+    '''
+    save_user method to save a user
+    '''
+    user.save_user()
+#The end!
+
+def display_user():
+    """
+    display_user method to display an existing user
+    """
+    return User.display_user()
+#The end!
+def signin_user(username,password):
+    """
+    signin_user method to check a user and then sign in they exist
+    """
+    check_user = User.find_by_name(username,password)
+    return find_user
+#The End!
+
+def create_credential(platform,email,password):
+    """
+    create_credential method to create new credentials
+    """
+    new_credential = Credentials(platform,email,password)
+    return new_credential
+#The end!
+
+
+if __name__ == '__main__':
+    password()
 
