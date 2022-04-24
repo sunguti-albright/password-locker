2833a7c86e04a6fc9947b1038e9b6faf2e3db099
albright.human <sungutialbright@gmail.com>
2022-04-24 03:40:02 +0300

    add modules and imports for the password locker

--- password.py
+++ password.py
@@ -1,1 +1,23 @@
-##password locker
+import pyperclip
+import random
+import string
+
+# class User:
+#     """
+#     Initiates new instances of a user
+#     """
+    
+#     user_list = []
+
+#     def __init__(self, user_name, password):
+#         """
+#         Initializes the user
+#         """
+#         self.user_name = user_name
+#         self.password = password
+        
+#     def save_user(self):
+# #         """Saves the user"""
+#         User.user_list.append(self)
+
+
